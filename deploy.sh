61e0a27cf60888a4658da4378a8de43e89d7d6ad
davidtazy <david.allemant@gmail.com>
2022-11-02 19:12:59 +0100

    co;plete the deploy script to have a one click action

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,16 @@
 #!/bin/bash
 set -ex
 
-rm -rf build
-npm run build
-tar -zcvf build.tgz build
+#rm -rf build
+#npm run build
+#tar -zcvf build.tgz build
+#scp  build.tgz pi@192.168.1.38:/home/pi/teleinfo
 
-scp  build.tgz pi@192.168.1.38:/home/pi/teleinfo
+echo "########## load on server ###########"
+
+ssh pi@192.168.1.38 "sudo systemctl stop teleinfo_ui.service  &&\
+ pwd && cd /home/pi/teleinfo &&  tar -zxvf build.tgz &&\
+ sudo systemctl start teleinfo_ui.service"
+
+echo "########## restart viewer ###########"
+ssh pi@192.168.1.34 "sudo reboot"

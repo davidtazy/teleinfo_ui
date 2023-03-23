5d3e71241c87c94396dac82e77563a0d6f2d4d0f
davidtazy <david.allemant@gmail.com>
2023-03-23 19:13:36 +0100

    responsive for bigger screen

--- src/Card.tsx
+++ src/Card.tsx
@@ -1,3 +1,5 @@
+import React, { useState, useEffect } from "react";
+
 export type CardProps = {
   value: string;
   unit: string;
@@ -7,47 +9,74 @@ export type CardProps = {
 };
 
 export function Card(props: CardProps) {
-  return (
-    <>
-      <div
-        style={{
-          margin: 5,
-          height: 10,
-        }}
-      />
-      <div
-        style={{
-          boxShadow: "0 4px 8px 0 rgba(0,0,0,0.2)",
-          backgroundColor: props.color,
-          fontSize: "25vh",
-          textAlign: "left",
-          margin: 5,
-          height: "100vh",
-        }}
-      >
-        {props.icon}
-
-        <p
-          style={{
-            fontSize: "1.6em",
-            margin: 0,
-            fontWeight: "bold",
-            textAlign: "center",
-          }}
-          id="val"
-        >
-          {props.value}
-          <span
-            style={{
-              fontSize: "0.5em",
-              fontWeight: "normal",
-              margin: 0,
-            }}
-          >
-            {" " + props.unit}
-          </span>
-        </p>
-      </div>
-    </>
+  const [matches, setMatches] = useState(
+    window.matchMedia("(min-width: 600px)").matches
   );
+
+  useEffect(() => {
+    window
+      .matchMedia("(min-width: 700px)")
+      .addEventListener("change", (e) => setMatches(e.matches));
+  }, []);
+
+  if (!matches) {
+    //small screen
+
+    return (
+      <>
+        <div
+          style={{
+            boxShadow: "0 4px 8px 0 rgba(0,0,0,0.2)",
+            backgroundColor: props.color,
+            fontSize: "25vh",
+            textAlign: "left",
+            margin: 5,
+            height: "100vh",
+          }}
+        >
+          {props.icon}
+
+          <p
+            style={{
+              fontSize: "1.6em",
+              margin: 0,
+              fontWeight: "bold",
+              textAlign: "center",
+            }}
+            id="val"
+          >
+            {props.value}
+            <span
+              style={{
+                fontSize: "0.5em",
+                fontWeight: "normal",
+                margin: 0,
+              }}
+            >
+              {" " + props.unit}
+            </span>
+          </p>
+        </div>
+      </>
+    );
+  } else {
+    //big screen
+    return (
+      <>
+        <div
+          style={{
+            boxShadow: "0 4px 8px 0 rgba(0,0,0,0.2)",
+            backgroundColor: props.color,
+            fontSize: "20vh",
+            margin: 5,
+            padding: 10,
+            height: "21vh",
+          }}
+        >
+          {props.icon} {props.value}
+          {" " + props.unit}
+        </div>
+      </>
+    );
+  }
 }
